01f6b929ce8322fc4b2ca367cc2fe8fe26daaec5
zischl <147017426+zischl@users.noreply.github.com>
2025-05-16 22:36:42 +0530

    *Tons of improvements and updates  - Value adding logic changes  - added text justification options for left, center, right  - improved item placement logic and auto resizing capabilities  - fixes for argument options border_width, corner_radius, font, option_outline, option_border_color, option_bg_color  - added hideScrollBar feature  - updates to padx, pady, ipadx, ipady for spacing for option set borders and text spacing  - added option_spacing argument to add spacing between options  - modified add method to work with rounded option corners  - fixed value multiAdd function to avoid initial loading times with any number of items by  dynamic adding using threading.

--- smth.py
+++ smth.py
@@ -4,12 +4,13 @@ import platform
 from collections import defaultdict 
 import threading
 
-
-                                   
 class addCustomDropdown(ctk.CTkToplevel):
-    def __init__(self, master, width=None, height=200, corner_radius=10, bg_color="#4a412a", border_width=0, border_color="white", values = (), 
-                 fg_color="#181818", text_color='gray', hover_color="#383838", hover_text_color="white", mouse_cursor="hand2", command=None, 
-                 font=('', 11), option_outline=0, option_border_color='', option_bg_color='#282828', **kwargs):
+    def __init__(self, master, width=None, height=200, corner_radius=10, 
+                 border_width=0, border_color="white", values = (), 
+                 fg_color="#181818", text_color='gray', hover_color="#383838", hover_text_color="white", 
+                 command=None, anchor='left', hideScrollBar=False,
+                 font=("Arial", 11, 'bold'), option_outline=0, option_border_color='', option_bg_color='', option_rounded_corners=25,
+                 option_spacing=10, padx=(0,0), pady=(0,0), ipadx=10, ipady=10, **kwargs):
         super().__init__(master)
         self.configure(fg_color="#4a412a", corner_radius=corner_radius)
         self.wm_overrideredirect(True)
@@ -19,24 +20,28 @@ class addCustomDropdown(ctk.CTkToplevel):
         elif _platform.startswith('win'):
             self.overrideredirect(True)
             self.wm_attributes("-transparentcolor", '#4a412a')
-            
+        
+        if "combo" in master.winfo_name():
+            master._dropdown_menu = self
+        
+        
         self.master = master
         self.width = width if width is not None else master.cget('width')
         self.height = height if height is not None else master.cget('height')
         self.items = {}
         self.itemLookup = defaultdict(list)
         self.command = command
-        self.optionHeight = 15
-        self.padx, self.pady = 10, 10
-        self.ipadx = 5
+        self.padx, self.ipadx, self.pady, self.ipady ,self.optionSpacing = padx, ipadx, pady, ipady, option_spacing
         self.hoverItem = 0
         self.hoverItemFrame = None
         self.borderWidth = border_width
         self.borderColor = border_color
         self.fg = fg_color
-        self.hoverColor, self.hoverTextColor = hover_color, hover_text_color
-        self.textColor = text_color
-        self.option_outline, self.option_border_color, self.option_bg_color = option_outline, option_border_color, option_bg_color
+        self.hoverColor, self.hoverTextColor, self.font = hover_color, hover_text_color, font
+        self.optionHeight = self.getOptionHeight()
+        self.textColor, self.anchor = text_color, 'nw' if anchor == 'left' else 'ne' if anchor == 'right' else 'center'
+        self.option_outline, self.option_border_color, self.option_bg_color, self.option_rounded_corners = option_outline, option_border_color, option_bg_color, option_rounded_corners
+        
         
         self.mainFrame = ctk.CTkFrame(self, width=self.width, height=self.height, corner_radius=corner_radius, border_width=border_width, border_color=border_color
                                    , fg_color=fg_color)
@@ -46,16 +51,18 @@ class addCustomDropdown(ctk.CTkToplevel):
         self.mainFrame.grid_rowconfigure(0, weight=1)
         self.mainFrame.grid_columnconfigure(0, weight=1)
         
-        self.mainCanvas = tk.Canvas(self.mainFrame, height=self.height, bg=self.fg, highlightbackground=self.fg)
-        self.mainCanvas.grid(row=0, column=0, sticky='nwse', pady=7)        
+        if not hideScrollBar:
+            self.scrollbar = ctk.CTkScrollbar(self.mainFrame, fg_color=fg_color, height=self.height)
+            self.scrollbar.grid(row=0, column=0, sticky='nes', pady=(max(7, border_width)+self.pady[0],max(7, border_width)+self.pady[1]), padx=(0, border_width))
+        self.scrollbarWidth = self.scrollbar.winfo_reqwidth() if not hideScrollBar else 0
         
-        self.scrollbar = ctk.CTkScrollbar(self.mainFrame, fg_color=fg_color, command=self.mainCanvas.yview, height=self.height)
-        self.scrollbar.grid(row=0, column=0, sticky='nes', pady=7) 
-        # self.scrollbar.lower()
+        self.mainCanvas = tk.Canvas(self.mainFrame, height=self.height, bg=self.fg, highlightbackground=self.fg)
+        self.mainCanvas.grid(row=0, column=0, sticky='nwse', pady=(max(7, border_width)+self.pady[0],max(7, border_width)+self.pady[1]), padx=(border_width+self.padx[0], border_width+self.scrollbarWidth+self.padx[1]))        
         self.mainCanvas.update()
-
-        self.mainCanvas.configure(yscrollcommand=self.scrollbar.set)
-        # self.mainCanvas.configure(height=500)
+        if not hideScrollBar:
+            self.scrollbar.configure(command=self.mainCanvas.yview)
+            # self.scrollbar.lower()
+            self.mainCanvas.configure(yscrollcommand=self.scrollbar.set)
 
         master.update()
         self.withdraw()
@@ -63,24 +70,39 @@ class addCustomDropdown(ctk.CTkToplevel):
         
         self.mainCanvas.bind("<Motion>", self.onHover)
         self.mainCanvas.bind("<MouseWheel>" ,self.mouseScroller)
+        self.mainCanvas.bind("<Button-1>", self.onClick)
         
         
         # btn_up = tk.Button(self.mainFrame, text="▲").pack(fill='x')
         # btn_down = tk.Button(self.mainFrame, text="▼").pack(fill='x')
         
         if values:
-            self.multiAdd(values)
+            self.addThreaded(values)
+            
+        if master._values:
+            self.addThreaded(master._values)
+            
+    def getOptionHeight(self):
+        canvas = tk.Canvas(self, width=50, height=50)
+        text = canvas.create_text(0, 0, text="Sample Text", font=self.font)
+        bbox = canvas.bbox(text)
+
+        optionHeight = bbox[3] - bbox[1]
+        return optionHeight
+            
+    def addThreaded(self, values):
+        threading.Thread(target=self.multiAdd, kwargs={'values':values}, daemon=True).start()
         
     def multiAdd(self, values):
         for item in values:
-            self.mainCanvas.after(0, lambda : self.add(item))
-    
+            self.add(option=item)
+
     def open(self, *args):
         self.geometry(f"{self.master.cget('width')}x{300}+{self.master.winfo_rootx()}+{self.master.winfo_height()+self.master.winfo_rooty()}")
         self.deiconify() if self.state() == 'withdrawn' else self.withdraw()
     
-    def createRoundedRectangle(self, x1, y1, x2, y2, radius=25, **kwargs):
-        
+    def createRoundedRectangle(self, x1, y1, x2, y2, **kwargs):
+        radius = self.option_rounded_corners
         points = [x1+radius, y1,
                 x1+radius, y1,
                 x2-radius, y1,
@@ -105,16 +127,25 @@ class addCustomDropdown(ctk.CTkToplevel):
         return self.mainCanvas.create_polygon(points, **kwargs, smooth=True)
     
     def add(self, option, command=None):
-        x, y = self.padx, len(self.items)*(5+self.optionHeight+self.pady*2)+self.pady
-        self.mainCanvas.create_rectangle(self.borderWidth+0, y+self.borderWidth, self.winfo_width()-self.borderWidth*2, (y+self.optionHeight+self.pady*2), 
-                                         outline=self.option_border_color, fill=self.option_bg_color, width=self.option_outline, tags=("optionFrame"))
-        item = self.mainCanvas.create_text(x, y+self.pady, text=option, fill=self.textColor, font=("Arial", 11), tags=("option"), anchor='nw')
+        y =  len(self.items)*(self.optionSpacing+self.optionHeight+self.ipady*2)+self.ipady
+        if self.option_rounded_corners != 0:
+            self.createRoundedRectangle(self.option_outline+0, y+self.option_outline, self.mainCanvas.winfo_width(), (y+self.optionHeight+self.ipady*2), 
+                                            outline=self.option_border_color, fill=self.option_bg_color, width=self.option_outline, tags=("optionFrame"))
+        else:
+            self.mainCanvas.create_rectangle(self.option_outline+0, y+self.option_outline, self.mainCanvas.winfo_width(), (y+self.optionHeight+self.ipady*2), 
+                                            outline=self.option_border_color, fill=self.option_bg_color, width=self.option_outline, tags=("optionFrame"))
+        if self.anchor == 'nw':
+            item = self.mainCanvas.create_text(self.ipadx, y+self.ipady, text=option, fill=self.textColor, font=self.font, tags=("option"), anchor=self.anchor)
+        elif self.anchor == 'center':
+            item = self.mainCanvas.create_text((self.mainCanvas.winfo_width()/2), y+self.ipady+self.optionHeight//2, text=option, fill=self.textColor, font=self.font, tags=("option"), anchor=self.anchor)
+        else:
+            item = self.mainCanvas.create_text(self.mainCanvas.winfo_width()-self.ipadx, y+self.ipady, text=option, fill=self.textColor, font=self.font, tags=("option"), anchor=self.anchor)
         self.items[len(self.items)] = item
         self.itemLookup[option].append(len(self.items))
         self.mainCanvas.configure(scrollregion=self.mainCanvas.bbox('all'))
             
     def onHover(self, event):
-        option = self.mainCanvas.find_closest(self.padx, self.mainCanvas.canvasy(event.y))
+        option = self.mainCanvas.find_closest(self.ipadx, self.mainCanvas.canvasy(event.y))
         if option in (self.hoverItem, self.hoverItemFrame): return
         
         tag = self.mainCanvas.gettags(option)[0]
@@ -134,10 +165,12 @@ class addCustomDropdown(ctk.CTkToplevel):
     def mouseScroller(self, event):
         self.mainCanvas.yview_scroll(int(-1*(event.delta/120)), "units")
 
-                
+    def onClick(self, event):
+        self.master.set(self.mainCanvas.itemcget(self.hoverItem, 'text'))
+    
 app = ctk.CTk()
-dropdown = ctk.CTkComboBox(app)
+dropdown = ctk.CTkComboBox(app, values=[f'bleh{_}' for _ in range(30000)], command=lambda: print('grg'))
 dropdown.pack()
-dropdown._dropdown_menu = addCustomDropdown(dropdown, values=['bleh' for _ in range(100)])
 
+addCustomDropdown(dropdown)
 app.mainloop()
